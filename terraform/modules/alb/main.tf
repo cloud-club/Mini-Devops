da96222bb0b9ff42a1bb536568ab925d0ab497de
jieun1128 <5656jieun@naver.com>
2022-05-08 15:36:06 +0900

    alb 수정

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,60 +1,54 @@
-# SG for ALB
-resource "aws_security_group" "mini-alb" {
-  name        = "mini-alb"
-  description = "Security group for ALB"
-  vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = "${var.ingress_cidr_blocks}"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-      Name = "${var.name}-alb-sg"
-  }
-}
-
-# Application load balancer
 resource "aws_lb" "mini-alb" {
-  name               = "test-lb-tf"
+  name               = "mini-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.mini-alb.id}"]
-  subnets            = [for subnet in var.subnet_id : subnet]
-
-  enable_deletion_protection = true
+  security_groups    = [
+    var.load_balancer_sg.id]
+  subnets            = [
+    var.load_balancer_subnet_a.id,
+    var.load_balancer_subnet_b.id]
 
   tags = {
-    Environment = "production",
-    Name = "${var.name}-alb"
+    Name = "mini-alb"
+    Project = "mini-alb"
+    Billing = "mini-alb"
   }
 }
 
-# target group
-resource "aws_lb_target_group" "mini_tg" {
-  name     = "${var.name}-tg"
+resource "aws_lb_target_group" "mini-tg" {
+  name     = "mini-tg"
   port     = 80
   protocol = "HTTP"
+  vpc_id   = var.vpc.id
   target_type = "ip"
-  vpc_id   = "${var.vpc_id}"
 
   health_check {
-    interval            = 30
+    enabled             = true
+    interval            = 300
     path                = "/"
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
+    timeout             = 60
+    matcher             = "200"
+    healthy_threshold   = 5
+    unhealthy_threshold = 5
   }
 
-  tags { Name = "Mini Target Group" }
+  tags = {
+    Name = "mini-tg"
+  }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.mini-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
--- terraform/modules/alb/variables.tf
+++ terraform/modules/alb/variables.tf
@@ -1,20 +1,7 @@
-variable "name" {
-  description = "모듈에서 정의하는 모든 리소스 이름의 prefix"
-  type = string
-  default = "mini"
-}
+variable "load_balancer_sg" {}
 
-variable "vpc_id"{
-  description = "ALB에서 이용할 VPC id"
-  type = string
-}
+variable "load_balancer_subnet_a" {}
 
-variable "ingress_cidr_blocks"{
-  description = "ALB에서 이용할 cidr blocks"
-  type = string
-}
+variable "load_balancer_subnet_b" {}
 
-variable "subnet_id"{
-  description = "ALB에서 이용할 Subnet들의 id"
-  type = list
-}
+variable "vpc" {}
