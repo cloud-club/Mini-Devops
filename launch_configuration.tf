8b73e9bd926403c0be3bc98e905967ae5a2bf02c
minnji88 <kkhhjj3393@gmail.com>
2022-05-09 12:50:17 +0900

    ami, instance type 설정

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -2,9 +2,9 @@ resource "aws_launch_configuration" "mini_configuration" {
     associate_public_ip_address = true
     
     iam_instance_profile = module.iam_instance_profile.ecs.ecsInstanceProfile
-    image_id = 
-    instance_type = 
-    key_name   = 
+    image_id = ami-0cbec04a61be382d9
+    instance_type = t2.micro
+    key_name  = key_name // 채워야함
     lifecycle {
         create_before_destroy = true
     }
