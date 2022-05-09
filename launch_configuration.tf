c6bfc0be7c7f2f31e970bc3e764824ffbf48e68f
minnji88 <kkhhjj3393@gmail.com>
2022-05-09 12:14:31 +0900

    launch_configuration 추가

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -1,4 +1,21 @@
-resource "aws_launch_configuration" "" {
+resource "aws_launch_configuration" "mini_configuration" {
+    associate_public_ip_address = true
+    
+    iam_instance_profile = module.iam_instance_profile.ecs.ecsInstanceProfile
+    image_id = 
+    instance_type = 
+    key_name   = 
+    lifecycle {
+        create_before_destroy = true
+    }
 
+    name_prefix = "lauch-configuration-"
+    root_block_device {
+        volume_size = 30
+        volume_type = "gp2"
+    }
+
+
+    security_groups = []
 }
 
