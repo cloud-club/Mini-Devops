0d08e70b2838e02e341ae1b06f6a74748dfcbebd
Jieun Park <49158155+jieun1128@users.noreply.github.com>
2022-05-09 14:51:13 +0900

    key pair 입력

--- key_pair.tf
+++ key_pair.tf
@@ -1,7 +1,10 @@
+chmod 400 mini-devops.pem
+ssh-keygen -y -f mini-devops.pem >> terraform_ec2_key.pub
+
 resource "aws_key_pair" "mini_key_pair" {
-    key_name = KEY_NAME
-    public_key = PubKey
+    key_name = mini-devops
+    public_key = "file(terraform_ec2_key.pub)"
     tags = {
-        "Name" = ""
+        "Name" = "mini-key-pair"
     }
 }
