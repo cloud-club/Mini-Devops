0605d1e76690ac8f4bca723011c1ca1625dcca29
minnji88 <kkhhjj3393@gmail.com>
2022-05-11 23:18:32 +0900

    sg 수정

--- terraform/modules/vpc/sg.tf
+++ terraform/modules/vpc/sg.tf
@@ -29,9 +29,9 @@ resource "aws_security_group" "load_balancer_sg" {
   description = "Mini_Devops Security Group"
 
   ingress {
-    from_port = 80
+    from_port = 8080
     protocol = "tcp"
-    to_port = 80
+    to_port = 8080
     cidr_blocks = ["0.0.0.0/0"]
   }
 
